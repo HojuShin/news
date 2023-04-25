6eedf2750421677c49443dd79bc11d3a958d13e1
HojuShin <shin03188@naver.com>
2023-04-25 20:40:59 +0900

    데이터 가져오기

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
 import { createSpinner } from "./spinner.js";
 
-function renderTopNews(){
+function renderTopNews() {
     const articleSection = document.getElementById('topNewsList');
 
     createSpinner(articleSection);
-}; 
-function renderLatestNews(){
+
+    //데이터 가져오기
+    fetch('./data/top.json')
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+};
+function renderLatestNews() {
     const latestNewsList = document.querySelector('.latest-news-list');
 
-    createSpinner(latestNewsList); 
+    createSpinner(latestNewsList);
+
+    //데이터 가져오기
+    fetch('./data/latest.json')
+        .then((res) => res.json())
+        .then((data) => console.log(data))
 };
 
 document.addEventListener('DOMContentLoaded', () => { //데이터 조회 시에는 리소스에 접근 X
