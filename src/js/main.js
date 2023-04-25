ffac0946fa9ca3dcf6c1e7f4468a1e629e8ba33c
HojuShin <shin03188@naver.com>
2023-04-25 22:27:31 +0900

    latestNews 영역

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,22 @@ function createTopNewsElement(article) {
     return anchor ;
 }
 
+//latestNews DOM 생성 
+function createLatestNewsElement(article) {
+    const { title, link } = article ;
+
+    const listItem = document.createElement('li');
+    const anchor = document.createElement('a');
+
+    anchor.setAttribute('href', link);
+    anchor.textContent = title ;
+
+    listItem.className = 'latest-news-item';
+    listItem.append(anchor);
+
+    return listItem;
+}
+
 //topNews 로딩이미지 영역 설정(spinner 인자 전달), 데이터 요청, 데이터 전달(createTopNewsElement인자 전달)
 function renderTopNews() {
     const articleSection = document.getElementById('topNewsList');
@@ -44,18 +60,25 @@ function renderTopNews() {
                 //articleSection(topNewsList) 밑으로 spinner, createTopNewsElement가 있음
             })
     }, 1500);
-
 };
 
+//latestNews  로딩이미지 영역 설정(spinner 인자 전달), 데이터 요청, 데이터 전달(createLatestNewsElement인자 전달)
 function renderLatestNews() {
     const latestNewsList = document.querySelector('.latest-news-list');
 
     createSpinner(latestNewsList);
 
-    //데이터 가져오기
-    fetch('./data/latest.json')
-        .then((res) => res.json())
-        .then((data) => console.log(data))
+     setTimeout(() => {
+        fetch('./data/latest.json')
+            .then((res) => res.json())
+            .then((data) => {
+                const { articles } = data;
+                //createLatestNewsElement함수 호출하여 데이터 할당 
+                const articleList = articles.map((article) => createLatestNewsElement(article));
+
+                latestNewsList.append(...articleList);
+            })
+    }, 1500);
 };
 
 document.addEventListener('DOMContentLoaded', () => { //데이터 조회 시에는 리소스에 접근 X
