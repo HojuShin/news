804dc5915fe682a42ea5cfebcdbad82f67095802
HojuShin <shin03188@naver.com>
2023-04-25 18:42:43 +0900

    DOMContentLoaded 이벤트 등록

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+function renderTopNews(){}; 
+function renderLatestNews(){};
+
+document.addEventListener('DOMContentLoaded', () => { //데이터 조회 시에는 리소스에 접근 X
+    renderTopNews();
+    renderLatestNews();
+})
